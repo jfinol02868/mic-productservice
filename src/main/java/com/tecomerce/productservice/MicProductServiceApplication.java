e2a1878cd11c4289b2de3f5628c5620c22f264c6
Jesus Finol <jfinol02@gmail.com>
2024-04-27 15:24:31 +0200

    Delete persistense class

--- src/main/java/com/tecomerce/productservice/MicProductServiceApplication.java
+++ src/main/java/com/tecomerce/productservice/MicProductServiceApplication.java
@@ -1,12 +1,8 @@
 package com.tecomerce.productservice;
 
-import com.tecomerce.productservice.infrastructure.bd.model.entity.ProductEntity;
-import com.tecomerce.productservice.infrastructure.bd.model.repository.ProductRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.List;
 
 @SpringBootApplication
 public class MicProductServiceApplication implements CommandLineRunner {
