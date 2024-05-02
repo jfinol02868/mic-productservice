312e970a7a77d6e9d7fa2edb1659b129d960261a
Jesus Finol <jefinol@hiberus.com>
2024-05-02 17:21:34 +0200

    Add configuration for cors

--- src/main/java/com/tecomerce/productservice/config/WebConfig.java
+++ src/main/java/com/tecomerce/productservice/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.tecomerce.productservice;
+package com.tecomerce.productservice.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
