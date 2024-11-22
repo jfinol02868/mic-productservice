44f1323753d000ed8a37775c6ec5e91b82e6e4e9
Jesus Finol <jefinol@hiberus.com>
2024-11-22 11:36:41 +0100

    Partial kafka config

--- src/main/java/com/solutionsone/mic/productservice/infrastructure/bd/kafka/KafkaConfig.java
+++ src/main/java/com/solutionsone/mic/productservice/infrastructure/bd/kafka/KafkaConfig.java
@@ -1,12 +1,9 @@
 package com.solutionsone.mic.productservice.infrastructure.bd.kafka;
 
-import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
-import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
-import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.StreamsConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
 import org.springframework.kafka.config.KafkaStreamsConfiguration;
@@ -14,27 +11,24 @@ import org.springframework.kafka.config.KafkaStreamsConfiguration;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.apache.kafka.streams.StreamsConfig.*;
+
+
+@EnableKafka
 @Configuration
 @EnableKafkaStreams
 public class KafkaConfig {
 
-    private static final String TOPIC_NAME = "products";
     private static final String bootstrapServers = "localhost:9092";
-    private String schemaRegistryUrl = "http://localhost:8081";
 
     @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
-    public KafkaStreamsConfiguration kafkaStreamsConfiguration() {
-        Map<String, Object> config = new HashMap<>();
-        config.put(StreamsConfig.APPLICATION_ID_CONFIG, "product-streams-app");
-        config.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        config.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistryUrl);
-        config.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-        config.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
-        return new KafkaStreamsConfiguration(config);
-    }
+    KafkaStreamsConfiguration kStreamsConfig() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(APPLICATION_ID_CONFIG, "streams-app");
+        props.put(BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        props.put(DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
 
-    @Bean
-    public NewTopic productsTopic() {
-        return new NewTopic(TOPIC_NAME, 3, (short) 1);
+        return new KafkaStreamsConfiguration(props);
     }
 }
