2434d725797eca28b475fef7f8d0f31767db7766
Jesus Finol <jefinol@hiberus.com>
2024-11-26 17:47:54 +0100

    Refactor of sonar

--- src/main/java/com/solutionsone/mic/productservice/api/GlobalExceptionHandler.java
+++ src/main/java/com/solutionsone/mic/productservice/api/GlobalExceptionHandler.java
@@ -18,7 +18,7 @@ import java.util.List;
 import java.util.Objects;
 
 @RestControllerAdvice
-public class ControlAdviceException {
+public class GlobalExceptionHandler {
 
 
     @ExceptionHandler(PageNotValidException.class)
--- src/main/java/com/solutionsone/mic/productservice/infrastructure/bd/util/DynamicSpecification.java
+++ src/main/java/com/solutionsone/mic/productservice/infrastructure/bd/util/DynamicSpecification.java
@@ -13,6 +13,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import static com.solutionsone.mic.productservice.domain.util.Message.ACCESS_FIELD_ERROR;
+
 public class DynamicSpecification {
 
     private DynamicSpecification() {}
@@ -25,14 +27,13 @@ public class DynamicSpecification {
             Field[] fields = filterObject.getClass().getDeclaredFields();
 
             for (Field field : fields) {
-                field.setAccessible(true);
                 try {
                     Object value = field.get(filterObject);
                     if (Objects.nonNull(value)) {
                         predicates.add(criteriaBuilder.equal(root.get(field.getName()), value));
                     }
                 } catch (IllegalAccessException e) {
-                    throw new AccessToFieldException("AC001", "Error al acceder al campo: ".concat(field.getName()));
+                    throw new AccessToFieldException(ACCESS_FIELD_ERROR.getCode(), String.format(ACCESS_FIELD_ERROR.getValue(), field.getName()));
                 }
             }
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
